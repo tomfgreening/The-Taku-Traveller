133d1e2fb3764730c9272e3b454f3bc068f06615
Thomas Greening <tomfgreening@gmail.com>
2025-03-22 21:04:48 +0000

    Experimenting with how best to implement a reveal of the Tokyo paragraph text with the learn more button.

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log("if you are reading this, the Javascript file has been successfully!
 const learnMoreButton = document.getElementById('learnMoreButton');
 const tokyoParagraph = document.getElementById('tokyoParagraph');
 
+learnMoreButton.addEventListener('click', function(){
+    tokyoParagraph
+}
+)
