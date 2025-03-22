8b84420aecd03ec82dede169b441c1f3bb562ed6
Thomas Greening <tomfgreening@gmail.com>
2025-03-22 20:55:32 +0000

    Targeting button and paragraph in the DOM using JS.

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("if you are reading this, the Javascript file has been successfully!");
+
+const learnMoreButton = document.getElementById('learnMoreButton');
+const tokyoParagraph = document.getElementById('tokyoParagraph');
+
