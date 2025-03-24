6c4ef8d7b265af11b2c69691ced3556393966b81
Thomas Greening <tomfgreening@gmail.com>
2025-03-24 13:42:48 +0000

    Implemented learn more button for Kyoto section.

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ console.log(
 
 const learnMoreButtonTokyo = document.getElementById("learnMoreButtonTokyo");
 const tokyoParagraph = document.getElementById("tokyoParagraph");
-const tokyTitleText = document.getElementById("tokyoTitleText");
+const tokyoTitleText = document.getElementById("tokyoTitleText");
 
 learnMoreButtonTokyo.addEventListener("click", function (event) {
   learnMoreButtonTokyo.style.display = "none";
   tokyoParagraph.style.display = "block";
   tokyoTitleText.style.display = "none";
+});
+
+const learnMoreButtonKyoto = document.getElementById("learnMoreButtonKyoto");
+const kyotoParagraph = document.getElementById("kyotoParagraph");
+const kyotoTitleText = document.getElementById("kyotoTitleText");
+
+learnMoreButtonKyoto.addEventListener("click", function (event){
+  learnMoreButtonKyoto.style.display="none";
+  kyotoParagraph.style.display="block";
+  kyotoTitleText.style.display="none";
 });
