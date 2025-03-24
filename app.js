2f16ba4a3ff66b62762695926fa68e2236b552d9
Thomas Greening <tomfgreening@gmail.com>
2025-03-24 13:14:43 +0000

    Added functionality to button that makes Tokyo title text disappear and be replaced with a paragraph of text.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 console.log(
-  "if you are reading this, the Javascript file has been successfully!"
+  "if you are reading this, the Javascript file has been successfully linked!"
 );
 
 const learnMoreButton = document.getElementById("learnMoreButton");
 const tokyoParagraph = document.getElementById("tokyoParagraph");
+const tokyTitleText = document.getElementById("tokyoTitleText");
 
 learnMoreButton.addEventListener("click", function (event) {
   learnMoreButton.style.display = "none";
   tokyoParagraph.style.display = "block";
+  tokyoTitleText.style.display = "none";
 });
